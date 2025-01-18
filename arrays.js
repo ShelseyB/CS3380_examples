60c8ebdabd45afd41447a223bce3cfe71605e0d0
shelseyb@gmail.com <shelseyb@gmail.com>
2025-01-18 16:09:35 -0700

    some changes for 1/21

--- arrays.js
+++ arrays.js
@@ -47,7 +47,7 @@ console.log("elmt", elmt)
 const idx = test_array.findIndex((item) => item == 100);
 console.log('idx', idx);
 
-const objArray = [
+const users = [
     {
         name: "Bob",
         age: 27
@@ -64,11 +64,95 @@ const objArray = [
         name: "Gertrude",
         age: 74
     },
+    {
+        name: "Betty",
+        age: 22
+    },
 ];
 
-const gertrude = objArray.findIndex(user => user.name == "Gertrude");
+const gertrude = users.findIndex(user => user.name == "Gertrude");
 console.log("gertrude", gertrude);
 
-// Subarray
-const sub = test_array.slice(1);
-console.log("sub", sub, test_array);
+// To find last
+const multiples = [1, 2, 1, 1];
+console.log("find last index", multiples.findLastIndex((num) => num == 1));
+console.log("find last", users.findLast((u) => u.age == 22));
+
+// // Subarray
+// const sub = test_array.slice(1);
+// console.log("sub", sub, test_array);
+
+// // Splice changes main array, removes sub array and returns it
+// const sub2 = test_array.splice(1, 3);
+// console.log("spliced", sub2, test_array);
+
+// // Adding to array
+// // Beginning:
+// console.log("unshift", test_array.unshift(12), test_array);
+// // End:
+// console.log("push", test_array.push(34), test_array);
+// // Both return resulting length
+// // Or, you can always use the spread operator
+// console.log([3, ...test_array]);
+// console.log([...test_array, 4]);
+// // NOTE: creates a new array
+
+// // Changing the length
+// sub2.length = sub2.length + 3;
+// console.log("sub2 increased", sub2);
+// sub2.length = sub2.length - 4;
+// console.log("sub2 decreased", sub2);
+
+// test_array.length = test_array.length + 1;
+// console.log(test_array);
+
+// // Every - ignores empty slots
+// console.log("every > 1", test_array.every((num) => num > 1));
+// console.log("every < 100", test_array.every((num) => num < 100));
+
+// // Some - returns "false" for empty slots
+// console.log("at least one > 1", test_array.some(it => it > 1))
+// console.log("at least one > 100", test_array.some(it => it > 100))
+
+// // Map - ignores empty slots
+// console.log("map", test_array.map(it => it * 2));
+// console.log(undefined * 2);
+
+// // Filter - returns an array that only contains the elements that satisfied the constraints
+// console.log("filter", test_array.filter((it) => it % 2 === 0));
+
+// // Reduce - Reduces the array to a single value by accumulating results.
+// console.log("reduce", test_array.reduce((sum, it) => sum + it), 0);
+
+// // Sort
+// console.log(test_array.sort());
+// // Custom sorting - return 1 if a should be before b, -1 if b should be before a
+// console.log(users.sort((a, b) => a.age < b.age ? -1 : 1));
+
+// // Reverse - reverses the array in place
+// test_array.push(78, 2);
+// test_array.unshift(1);
+// console.log(test_array);
+// test_array.reverse();
+// console.log(test_array);
+
+// // Concat - join multiple arrays
+// console.log([5, 6].concat([9, 10]).concat([2, 3]));
+
+// // Flat - take multi-dimensional array and flatten to single dimensional array
+// console.log([[1, 40], [5, [6, 8, 9]]].flat(1)); // Parameter is the depth to flatten
+// console.log([[1, 40], [5, [6, 8, 9]]].flat(2));
+// console.log([[1, 40], [5, [6, 8, [9]]]].flat(Infinity));
+
+// // Flat Map - map and flatten
+// console.log([1, 2, 3].map(num => [num, num * 2]));
+// console.log([1, 2, 3].flatMap(num => [num, num * 2]));
+
+// // Fill - fill array with static value
+// console.log([[1, 40], [5, [6, 8, 9]], 78].fill(0));
+
+// // Array.from - creates array from an iterable
+// console.log(Array.from("Hello"));
+
+// // Array.isArray
+// console.log(Array.isArray(test_array));
---
+++ type_coercion.js
@@ -0,0 +1,77 @@
+// The following values are "falsy"
+
+console.log("Boolean(0)", Boolean(0));
+console.log("Boolean('')", Boolean(''));
+console.log("Boolean(undefined)", Boolean(undefined));
+console.log("Boolean(null)", Boolean(null));
+console.log("Boolean(NaN)", Boolean(NaN));
+// Everything else is "truthy"
+
+// Even if a variable has a value, if it's one of these, if statements won't get triggered
+const value = "";
+if (value) {
+    console.log("Truthy")
+} else {
+    console.log("Falsy");
+}
+
+// Convert to Boolean
+if (!!value) {
+    console.log("Truthy", !!value)
+} else {
+    console.log("Falsy", !!value);
+}
+
+// String Concatenation
+console.log('5' + 2);    // "52" (string concatenation)
+console.log('Hello' + 1) // "Hello1"
+console.log(2 + 2 + '5') // "45" (2 + 2 is 4, then coerced to string and concatenated)
+
+// undefined and null in arithmetic
+// undefined gets coerced to NaN
+console.log(undefined + 5);
+console.log(undefined / 10);
+// null gets coerced to 0
+console.log(null + 5);
+console.log(null / 10);
+
+// Comparisons with ==
+console.log("null == 0", null == 0);
+console.log("null == undefined", null == undefined)
+console.log("0 == ''", 0 == ''); 
+console.log("'0' == 0", '0' == 0);
+
+// Converting objects to primitives
+const obj = { value: 10 };
+console.log(obj + 5); // "[object Object]5"
+
+const customObj = {
+  valueOf() {
+    return 42;
+  },
+  toString() {
+    return "string"
+  }
+};
+console.log(customObj + 5); // 47
+console.log(customObj + " now")
+
+// Array/Object to strings
+// Arrays get converted to strings when added
+console.log("[1, 2] + [3, 4]", [1, 2] + [3, 4]);
+console.log("[1, 2] - [3, 4]", [1, 2] - [3, 4]);
+console.log("[1, 2] * [3, 4]", [1, 2] * [3, 4]);
+console.log("[1, 2] / [3, 4]", [1, 2] / [3, 4]);
+
+/** Best Practices!
+ * 1. Use === and !== over == and !=
+ * 2. Use explicit type conversion String(), Number(), or Boolean()
+ * 3. Avoid Mixed-Type Operations:
+ *  - Try to keep operand types consistent (e.g., avoid adding numbers to strings).
+ * 4. Validate Inputs/Types:
+ */
+if (typeof value === 'number') {
+    console.log(value * 2);
+} else {
+    console.error('Invalid input type');
+}
