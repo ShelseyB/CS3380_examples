575946c1940c0cbb8a07caba12fae0e91539ffe8
shelseyb@gmail.com <shelseyb@gmail.com>
2025-04-15 21:01:24 -0600

    after class 4/15

--- basic_framework/js/main.js
+++ basic_framework/js/main.js
@@ -1,12 +1,9 @@
 let main = document.getElementById("main");
 
-
 let initialize = () => {
     let rtn = createHomePage();
     main.innerHTML = rtn.element;
     rtn.afterCreate();
 }
-
-
 
 initialize();
