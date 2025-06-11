e22361eeb64c7fe33223fc97c405aa121362451b
Shelsey <shelseyb@gmail.com>
2025-06-11 15:03:31 -0600

    updates after json video

--- json_example.js
+++ json_example.js
@@ -1,4 +1,16 @@
-const json_example = `{
+const obj = {
+    name: "Bob",
+    age: 35,
+    is_student: false,
+    address: {
+        street: "Harmony Ave",
+        number: 45
+    },
+    children: ["Martha", "Betty"],
+    spouse: null
+};
+
+const objJSON = `{
     "name": "Bob",
     "age": 35,
     "is_student": false,
@@ -6,15 +18,30 @@ const json_example = `{
         "street": "Harmony Ave",
         "number": 45
     },
-    "children": ["Martha", "Betty"]
+    "children": ["Martha", "Betty"],
+    "spouse": null
 }`;
 
-const js_obj = JSON.parse(json_example);
-console.log(json_example, js_obj, js_obj["addres"]?.["street"]);
+// Convert JSON to object
+const objJSONToObject = JSON.parse(objJSON);
+console.log(objJSON, objJSONToObject);
+
+// Convert object to JSON
+const objToJSON = JSON.stringify(obj);
+console.log(objToJSON, obj);
 
 fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then(response => {
-        // console.log("response", response, response.json);
-        return response.json();
+    .then(resp => {
+        // console.log("resp", resp);
+        return resp.json();
     })
-    .then(data => console.log(data));
+    .then(data => {
+        console.log(data);
+    })
+
+/**
+const user = { name: "Alice", age: 30 };
+  localStorage.setItem('user', JSON.stringify(user));
+  const retrievedUser = JSON.parse(localStorage.getItem('user'));
+  console.log(retrievedUser.name); // Output: Alice
+ */
