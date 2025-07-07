45a8840912e1a0cc5629d0b91cee9eba58a2b32f
Shelsey <shelseyb@gmail.com>
2025-07-07 14:13:50 -0600

    adds how to add env vars

--- envvars/envvars.js
+++ envvars/envvars.js
@@ -1,6 +1,23 @@
-console.log("MY_VAR", process.env.MY_VAR);
+/* Setting environment varibles */
+// macOS/Linux
+/**
+ * export MY_VARIABLE="Hello, World!"
+ * unset MY_VARIABLE
+ */
 
-require("dotenv").config();
+/**
+ * Windows
+ * set MY_VARIABLE=Hello, World!
+ * set MY_VARIABLE=
+ */
+/** Powershell
+ * $env:MY_VARIABLE="Hello, World!"
+ * Remove-Item Env:\MY_VARIABLE
+ */
+console.log("MY_VAR", process.env.MY_VAR, process.env.LANG || "en");
+
+import dotenv from "dotenv";
+dotenv.config();
 console.log("secret 1", process.env.SECRET_ONE);
 console.log("secret 2", process.env.SECRET_TWO);
 console.log("node env", process.env.NODE_ENV);
