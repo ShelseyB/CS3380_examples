3b4c8bb1fea62faa5d949d13136adc96829a0e8f
Shelsey <shelseyb@gmail.com>
2025-07-17 14:11:44 -0600

    updates to fetch

--- fetch.js
+++ fetch.js
@@ -1,21 +1,30 @@
-/** Get
+const url = "https://meowfacts.herokuapp.com/?count=3";
+
+/**
+ * HTTP Methods: 
+ * Get
  * Put
  * Post
  * Delete
  */
-
-const url = "https://meowfacts.herokuapp.com/?count=3";
-
-
-/** other ways to process:
- * .arrayBuffer()
- * .blob()
- * .formData()
- * .text()
- */
 // fetch(url, {
-//     method: "GET",  
-// }).then(response => response.json())
+//     method: "GET",
+//     // body: JSON.stringify({ username: "example" }),
+//     // headers: {
+//     //     "Content-Type": "application/json",
+//     //     "Authorization": "Bearer <token>"
+//     // }
+// })
+//     /** other ways to process:
+//      * .arrayBuffer()
+//      * .blob()
+//      * .formData()
+//      * .text()
+//      */
+//     .then(response => {
+//         console.log(response);
+//         return response.json();
+//     })
 //     .then(data => console.log(data))
 //     .catch(err => console.log("error fetching", err));
 
@@ -23,39 +32,14 @@ async function callFetch() {
     const catFacts = await fetch(url, {
         method: "GET",
     });
-    console.log(catFacts);
     if (!catFacts.ok) {
         console.log("something went wrong", catFacts.status);
+    }else {
+        console.log(catFacts.status)
     }
 }
 
-// callFetch();
+callFetch();
 
-const baseURL = "https://opentdb.com/api.php?amount=3";
-const queries = ["&category=20&type=multiple", "&category=20&difficulty=medium"];
+// axios is alternative
 
-const waitAndGetNext = async (results, queries) => {
-    if (queries.length) {
-        const fetchProm = new Promise((res, rej) => {
-            fetch(`${baseURL}${queries.pop()}`)
-                .then(r => r.json())
-                .then((r) => {
-                    setTimeout(async () => {
-                        console.log("r", r);
-                        const fetchRes = await waitAndGetNext([...results, r], queries);
-                        console.log("fetchRes", fetchRes);
-                        res([...results, r]);
-                    }, 5010);
-                });
-        });
-        const fetchRes = await fetchProm;
-        return fetchRes;
-    }
-}
-
-const getTrivia = async () => {
-    let results = await waitAndGetNext([], queries);
-    console.log("results", results);
-}
-
-getTrivia();
